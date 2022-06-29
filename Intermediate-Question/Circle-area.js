54f5d2afbcdc44d5b7764dabd2e76eb625bf6a06
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-29 20:43:42 +0530

    area of a circle

--- Intermediate-Question/Circle-area.js
+++ Intermediate-Question/Circle-area.js
@@ -1,3 +1,9 @@
+// Question no. -2 :Write a program to print the area of a circle by taking the radius as input.
+
+// Constraints
+// radius is an integer >= 0
+
+
 const calculateArea = (radius) => {
     let x = radius;
           return( Math.PI * x * x);
