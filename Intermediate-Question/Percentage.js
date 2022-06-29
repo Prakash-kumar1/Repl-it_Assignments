54c450834ae70b36574b16eb39fd35477b22e139
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-29 21:39:13 +0530

    Calculating Percentage

--- Intermediate-Question/Percentage.js
+++ Intermediate-Question/Percentage.js
@@ -1,6 +1,10 @@
+// Question no. - 5: Take input a as a number and b as percentage. Write a program to calculate the percentage of a number.
+
+
 function percent(num, percentage) {
     let x = (num*percentage)/100 ;
     return x ;
   }
   console.log(percent(400, 50))
+  console.log(percent(20, 5))
   exports.percent = percent
