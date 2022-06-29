91f9e5af6fe3f428ac7bf75a5372ff8eb5fd4745
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-29 21:29:55 +0530

    Calculating  difference

---
+++ Intermediate-Question/Difference.js
@@ -0,0 +1,9 @@
+// Question no.-3:Write a JavaScript program to get the difference between a given number and 13.
+
+
+const difference = (num) => {
+    let x = num - 13 ;
+    return x ;
+  }
+  console.log(difference  (12))
+  exports.difference = difference
