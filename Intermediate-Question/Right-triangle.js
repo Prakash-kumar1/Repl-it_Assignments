3aed8e0214e03e3fecc0fc5e4e54cf38c2881f8d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-29 19:25:14 +0530

    Finding hypotenuse

--- Intermediate-Question/Right-triangle.js
+++ Intermediate-Question/Right-triangle.js
@@ -1,3 +1,6 @@
+// Question No.-1 : Take two numbers as input perpendicular and base respectively and write a program to print the hypotenuse of the right angle triangle.
+
+
 const hypotenuse = (perpendicular, base) => {
     let  hypo = Math.sqrt((perpendicular**2)+ (base**2));
      return  hypo ;
