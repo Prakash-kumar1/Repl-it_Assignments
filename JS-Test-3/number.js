496e4b92302b6d92a8d9ebe0512680c9f5943d5d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-04 22:44:20 +0530

    Aircampus number

--- JS-Test-3/number.js
+++ JS-Test-3/number.js
@@ -1,3 +1,38 @@
+// Question No.-1 : Take an integer and print 'aircampus' if the input integer is an Aircampus number, otherwise print 'no'.
+
+// Hint:
+
+// A number is called Aircampus number if the sum of its own digits each raised to the power of the number of digits is equal to the original number itself.
+
+// Example:
+
+// 123 => 1^3 + 2^3 + 3^3 = 1 + 8 + 27 = 36
+
+// 1234 => 1^4 + 2^4 + 3^4 + 4^4 = 1 + 16 + 81 + 256 = 354
+
+// Constraints:
+
+// 0 < N < 10^6
+
+// Input:
+
+// A single integer is provided as input.
+
+// Output:
+
+// Print aircampus or no
+
+// Example:
+
+// Input:
+
+// 123
+
+// Output:
+
+// no
+
+
 function aircampus(n){
     let number = n
     let length = n.toString().length
