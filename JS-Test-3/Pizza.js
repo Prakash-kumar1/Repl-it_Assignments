7254b6469dbdea90bbd326e3e72eab813b30d0fc
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-04 22:49:50 +0530

    pizza

---
+++ JS-Test-3/Pizza.js
@@ -0,0 +1,40 @@
+function inhotel(Callback){
+    Callback();
+}
+
+function orderfood(){
+    console.log('step1:order a pizza');
+}
+function processing(){
+    console.log('step2: pizza order processing');
+}
+
+function  ingredeinats(){
+    console.log('step3: ingredeinats ready ');
+}
+
+
+function falvoured (){
+    console.log('step4: falvoured are added');
+}
+
+
+function cooked (){
+    console.log('step5: cooked and backed');
+}
+function delivery(){
+    console.log('step6: ready and out for delivery');
+}
+
+function pizaa(){
+    console.log('step7: pizaa received and ate it');
+}
+
+
+inhotel( orderfood)
+inhotel( processing)
+inhotel(  ingredeinats)
+inhotel( falvoured )
+inhotel( cooked)
+inhotel( delivery)
+inhotel( pizaa)
