9f0e6a10f7cb817aa644dd7267ae3742f66e2490
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-04 22:47:38 +0530

    Checking Number with Two different methods.

--- JS-Test-3/number.js
+++ JS-Test-3/number.js
@@ -52,4 +52,29 @@ function aircampus(n){
   }
   console.log(aircampus(123))
   exports.aircampus = aircampus
+
+
+  //       OR  Use this way
+
+function aircampus(n){
+  let original = n, count = 0, sum = 0;
+  // Calculate the number of digits
+  while(n!=0){
+    n = Math.floor(n/10);
+    count++;
+  }
+  console.log(n);
+  n= original; // After running the first loop n will become 0 so we are restoring it to original number
+  while(n > 0) {
+    let digit = n % 10;
+    n = Math.floor(n/10);
+    sum += digit ** count;
+    
+  }
+  console.log(n);
+  return original === sum ? 'aircampus':'no' ;
+}
+
+console.log(aircampus(145));
+exports.aircampus = aircampus
   
