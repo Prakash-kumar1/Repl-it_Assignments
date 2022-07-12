b0e35d3914bbaade878668378931c1f55c384e1d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-12 17:48:51 +0530

    return the 4th element from the Array.

---
+++ Array.js/element.js
@@ -0,0 +1,15 @@
+// Question Number-1 : Create an Array with the following elements: “Bike”, “Scooter”, “Car”, “Truck”, “JCB” , “Helicopter” and return the 4th element from the Array.
+
+// Input Format :  Not Applicable
+
+// Output Format :  4th Element from the Array
+
+// Sample Output :  Truck
+
+
+function createArray(){
+    let vechicles = ['Bike ', 'Scooter' , 'Car' , 'Truck' ,' JCB ', 'Helicopter']
+    return vechicles[3] ;
+  }
+  console.log(createArray())
+  exports.createArray = createArray
