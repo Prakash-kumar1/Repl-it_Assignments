1060dde9d3ccc1dd6d31698d9b03e8a78bdb5f95
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-12 21:12:54 +0530

    return the array in reverse order.

---
+++ Array.js/reverse.js
@@ -0,0 +1,14 @@
+// Question Number-9: Given an array, return the array in reverse order.
+// Input Format : An array arr
+
+// Output Format : Return array in reverse order
+
+// Sample Input: [1, 2, 3]
+// Sample Output: [3, 2, 1]
+
+
+function reverseArray(arr){
+    return arr.reverse()
+    }
+    console.log(reverseArray([1, 2, 3]))
+    exports.reverseArray= reverseArray
