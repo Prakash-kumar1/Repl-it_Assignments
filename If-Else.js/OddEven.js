e1075e5eb90d13f63fd8d078ed57dde9e60d4e47
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-17 23:07:27 +0530

    Given a number, determine whether it is odd or even.

---
+++ If-Else.js/OddEven.js
@@ -0,0 +1,26 @@
+// Question no-5: Given a number, determine whether it is odd or even.
+
+// Input: Input is provided as functional parameter.
+
+// Output: Return the output.
+
+// Eg 1: 5
+
+// output: odd
+
+// Eg 2: 8
+
+// output : even
+
+function odd_even(num){
+    // write your code here
+  if (num%2 == 0){
+    return ("even")
+  } else {
+    return ("odd") 
+  }
+    // return the answer
+  }
+  console.log(odd_even(5))
+  exports.odd_even = odd_even
+  
