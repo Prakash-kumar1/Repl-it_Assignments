965188c16e536154a4aab84354256fe840ca66e5
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-18 13:25:09 +0530

    check whether the last digit of a number is divisible by 2 or not.

---
+++ If-Else.js/last.js
@@ -0,0 +1,24 @@
+// Question no-18: Write a program to check whether the last digit of a number is divisible by 2 or not.
+
+// Input: Input is provided as functional parameter.
+
+// Output: Return the answer string.
+
+// Eg 1: 10
+// Output: yes
+
+// Eg 2: 7
+// Output : no
+
+
+function last_digit(num){
+    // write your code here
+    let last = Math.floor(num%10)
+  if (last%2 == 0){
+    return "yes"
+  } else {
+    return "no"
+  }
+  }
+  console.log(last_digit(7))
+  exports.last_digit = last_digit
