0eb3a3b40595018fe19a92682660f5f3c251abfd
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:36:16 +0530

    Create neighbour.js

---
+++ JS-Test-4/neighbour.js
@@ -0,0 +1,8 @@
+// Question Number -6: Mary wants to distribute pens to all the kids in her neighbourhood. Can you help her by creating an array where all the kids except her neighbour get 'm' number of pens and her neighbour kids get 'm+5'. Please note she doesn't have a kid.
+
+// Input Format : Three numbers m ,n and pos. Here m is the number of pens, n is the number of houses with kids and pos is where Mary lives.
+
+// Output Format : Return the output array.
+
+// Sample Input:  m = 5 n=10 pos = 2
+// Sample Output:  [5,10,0,10,5,5,5,5,5,5]
