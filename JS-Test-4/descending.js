dcc9faa4bd2389e9818dddc4c2ff970b65411dae
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:59:59 +0530

    Create descending.js

---
+++ JS-Test-4/descending.js
@@ -0,0 +1,10 @@
+Question Number -13: Given an array, return the descending order
+Input Format: An array arr
+
+Output Format: Return the array in descending order
+
+Sample Input:  [1, 5, 7, 8]
+Sample Output:  [8,7,5,1]
+
+Sample Input:  [1, 2, 3, 4, 3]
+Sample Output:  [4,3,3,2,1]
