415337664fe29605c7630fcc2a3a13ec283ff98a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:19:07 +0530

    Create equal.js

---
+++ JS-Test-4/equal.js
@@ -0,0 +1,13 @@
+// Question Number -1 : Given two arrays. Check if both arrays are equal or not.
+// An equal array is one which has equal frequencies of elements. If both the arrays are equal return true else false
+
+// Input Format : - Two arrays arr1 and arr2
+
+// Output Format : - Return true if the arrays are return false
+
+// Sample Input: [1, 5, 7, 8]  [5, 8, 7, 1]
+// Sample Output: true
+
+
+// Sample Input: [1, 2, 3]  [2,4,6]
+// Sample Output:  false
