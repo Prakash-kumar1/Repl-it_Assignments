f38565af1454157030120d9614d3f2dc1dc44b2b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:41:01 +0530

    Update position.js

--- JS-Test-4/position.js
+++ JS-Test-4/position.js
@@ -9,3 +9,19 @@ Sample Output:  [13,8]
 
 Sample Input:  [1, 2, 3]
 Sample Output:  [2,4]
+
+
+function odd_even_position(arr) {
+    let oddsum = 0, evensum = 0;
+    for (i = 0; i < arr.length; i++) {
+      if (i% 2 == 0) {
+        evensum += arr[i]
+      }
+      else {
+        oddsum += arr[i]
+      }
+    }
+    return [oddsum,evensum]
+  }
+  console.log(odd_even_position([1,5,7,8]))
+exports.odd_even_position = odd_even_position
