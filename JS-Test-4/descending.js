1822b98265e06830f20d2d04eec47b7a2d829190
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 12:00:52 +0530

    Given an array, return the descending order

--- JS-Test-4/descending.js
+++ JS-Test-4/descending.js
@@ -1,10 +1,19 @@
-Question Number -13: Given an array, return the descending order
-Input Format: An array arr
+// Question Number -13: Given an array, return the descending order
+// Input Format: An array arr
 
-Output Format: Return the array in descending order
+// Output Format: Return the array in descending order
 
-Sample Input:  [1, 5, 7, 8]
-Sample Output:  [8,7,5,1]
+// Sample Input:  [1, 5, 7, 8]
+// Sample Output:  [8,7,5,1]
 
-Sample Input:  [1, 2, 3, 4, 3]
-Sample Output:  [4,3,3,2,1]
+// Sample Input:  [1, 2, 3, 4, 3]
+// Sample Output:  [4,3,3,2,1]
+
+
+function descending_order(arr){
+    let result = arr.sort().reverse();
+    return result ;
+  }
+  console.log(descending_order([1, 5, 7, 8]))
+  console.log(descending_order([1, 2, 3, 4, 3]))
+  exports.descending_order = descending_order
