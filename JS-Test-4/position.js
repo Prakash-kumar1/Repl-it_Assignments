32893204093b1c3242a3f988efffd9f250eb76f4
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:40:16 +0530

    Create position.js

---
+++ JS-Test-4/position.js
@@ -0,0 +1,11 @@
+Question Number -7: Given an integer array, and a number ‘n’, please return the sum of integers at odd position and sum of integers at even position.
+
+Input Format:-  An array arr
+
+Output Format: Return array where first element is sum of odd elements & second element is sum of even elements.
+
+Sample Input:  [1, 5, 7, 8]
+Sample Output:  [13,8]
+
+Sample Input:  [1, 2, 3]
+Sample Output:  [2,4]
