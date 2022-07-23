6a13801d28fde6fc27c7555ad751903c61c28b8a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:26:24 +0530

    Create operation.js

---
+++ JS-Test-4/operation.js
@@ -0,0 +1,27 @@
+// Question Number -3: Given an array, the array has elements and operation to be performed. The operations are ADD_END’, REM_END’, ‘ADD_BEG’,’REM_BEG’. Create a new array by performing the given operations after each element. Return the resulting array.
+
+// Input Format : An array arr
+
+// The array contains elements and operations to be performed on the array
+
+// Opearations -: 
+
+// ADD_BEG : Add element to the beginning of the array
+// ADD_END: Add element to the end of the array
+// REM_BEG: Remove element from the beginning of the array
+// REM_END: Remove element from the end of the array.
+// Before every ADD operation there will be an element.
+
+// Output Format
+// Perfrom the operations given in the array and return the resulting array.
+
+
+// Sample Input:  [2, 'ADD_BEG', 3, 'ADD_BEG', 'REM_BEG', 4, 'ADD_END']
+// Sample Output:  [2, 4]
+
+
+// Sample Input:  [2, 'ADD_BEG', 'REM_BEG', 3, 'ADD_BEG', 'REM_BEG', 4, 'ADD_END', 'REM_BEG']
+// Sample Output: []
+
+
+
