6e4d6e965123c81fd7cfccfe020066e14ebc5890
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:47:48 +0530

    Create change.js

---
+++ JS-Test-4/change.js
@@ -0,0 +1,20 @@
+// Question Number -9: Given an integer array, pos array and element array, change the elements in the integer array based on the pos array and element array.
+
+// Input Format: Three input arrays, arr,pos,ele
+
+// Output Format: Return the output array after changing the elements
+
+// Sample Input:  arr = [1, 5, 7, 8]
+//                 pos = [0]
+//                 ele = [4]
+// Sample Output: [4, 5, 7, 8]
+
+
+// Sample Input: arr = [1, 2, 3]
+//                pos = [2,1]
+//                ele = [-1,5]
+// Sample Output: [1, 5, -1]
+
+// Note :- The position mentioned in the pos array will always be less than arr array.
+
+
