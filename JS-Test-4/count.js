79507468227d3697d4698c94df649972d33e9152
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:44:23 +0530

    Update count.js

--- JS-Test-4/count.js
+++ JS-Test-4/count.js
@@ -1,10 +1,11 @@
-Given an integer array, find all the elements which are divisible by the index position in which they are present. Consider the index of array starts from 1
+// Question Number -8: Given an integer array, find all the elements which are divisible by the index position in which they are present. Consider the index of array starts from 1
 
-Input Format : An array arr
+// Input Format : An array arr
 
-Output :Return number of integers which are divisible by its index (Assume that the index start from 1 and not 0)
+// Output :Return number of integers which are divisible by its index (Assume that the index start from 1 and not 0)
 
-Sample Input: [1, 5, 7, 8]
-Sample Output: 2
+// Sample Input: [1, 5, 7, 8]
+// Sample Output: 2
 
-Explanation: Consider that the index starts from 1 so, 1%1=0 , 5%2 = 1, 7%3 = 1, 8%4 = 0 , only 2 elements are divisible by their index
+// Explanation: Consider that the index starts from 1 so, 1%1=0 , 5%2 = 1, 7%3 = 1, 8%4 = 0 , only 2 elements are divisible by their index
+
