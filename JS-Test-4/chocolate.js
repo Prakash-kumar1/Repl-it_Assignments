412acd1a55a15fa4a52f7d4cd6fab691c013d309
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:33:30 +0530

    Create chocolate.js

---
+++ JS-Test-4/chocolate.js
@@ -0,0 +1,10 @@
+// Question Number -5: Unlce Ben decides to give chocolates to kids in his neighbourhood. He predecides the number of chocolates he wants to give each kid and arranges them in a bag. Now he wants to give 2 more chocolates to kids who less than 5 and reduce 1 chocolate who got more than 10.
+
+// Input Format : An array arr
+
+// Output Format : Return the array after correcting the number of chocolates.
+
+// Sample Input: [1, 6, 7, 11]
+// Sample Output: [3,6,7,10]
+
+
