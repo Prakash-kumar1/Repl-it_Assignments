041e60f1638b0020a5dab4e31de0c41ae21ea8a2
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:42:02 +0530

    the sum of integers at odd position and sum of integers at even position

--- JS-Test-4/position.js
+++ JS-Test-4/position.js
@@ -1,14 +1,14 @@
-Question Number -7: Given an integer array, and a number ‘n’, please return the sum of integers at odd position and sum of integers at even position.
+// Question Number -7: Given an integer array, and a number ‘n’, please return the sum of integers at odd position and sum of integers at even position.
 
-Input Format:-  An array arr
+// Input Format:-  An array arr
 
-Output Format: Return array where first element is sum of odd elements & second element is sum of even elements.
+// Output Format: Return array where first element is sum of odd elements & second element is sum of even elements.
 
-Sample Input:  [1, 5, 7, 8]
-Sample Output:  [13,8]
+// Sample Input:  [1, 5, 7, 8]
+// Sample Output:  [13,8]
 
-Sample Input:  [1, 2, 3]
-Sample Output:  [2,4]
+// Sample Input:  [1, 2, 3]
+// Sample Output:  [2,4]
 
 
 function odd_even_position(arr) {
@@ -24,4 +24,5 @@ function odd_even_position(arr) {
     return [oddsum,evensum]
   }
   console.log(odd_even_position([1,5,7,8]))
+  console.log(odd_even_position([1,2,3]))
 exports.odd_even_position = odd_even_position
