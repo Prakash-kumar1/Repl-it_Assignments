bedb91750bca5df7273210dd2f1abcc85e25c835
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:37:28 +0530

    Mary wants to distribute pens to all the kids in her neighbourhood

--- JS-Test-4/neighbour.js
+++ JS-Test-4/neighbour.js
@@ -6,3 +6,24 @@
 
 // Sample Input:  m = 5 n=10 pos = 2
 // Sample Output:  [5,10,0,10,5,5,5,5,5,5]
+
+
+function neighbours(m,n,pos){
+    let givePens = [] ;
+   
+   for(let i=0; i<n; i++){
+     givePens.push(m);
+   if(givePens[i] == givePens[pos]){
+     givePens.splice(i,1,0)
+   }
+     if(givePens[i] == givePens[pos - 1]){
+     givePens.splice(i,1,m+5)
+   }
+     if(givePens[i] == givePens[pos + 1]){
+     givePens.splice(i,1,m+5)
+   }
+   }
+     return givePens ;
+   }
+   console.log(neighbours(1,5,2))
+   exports.neighbours = neighbours
