36f01b71289feb013bcd9a242565bb389c677728
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:45:20 +0530

    elements which are divisible by their own  index position

--- JS-Test-4/count.js
+++ JS-Test-4/count.js
@@ -9,3 +9,16 @@
 
 // Explanation: Consider that the index starts from 1 so, 1%1=0 , 5%2 = 1, 7%3 = 1, 8%4 = 0 , only 2 elements are divisible by their index
 
+function count_all_divisions(arr){
+    let count = 0;
+    let index = 1; 
+      for(let i=0; i<arr.length; i++){
+        if(arr[i] % index == 0){
+          count++ ;
+        }
+        index++ ;
+      }
+      return count++
+    }
+    console.log(count_all_divisions([1, 5, 7, 8]))
+    exports.count_all_divisions = count_all_divisions
