6b4a72ef5bd89198e7d3396eae35fb6c030cc472
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:51:32 +0530

    Create square.js

---
+++ JS-Test-4/square.js
@@ -0,0 +1,11 @@
+Question Number -10: Given an array, start index and an end index, square the elements in the given range and return the array
+
+Input Format: An array arr, start index start and end index end
+Output Format: Return the subarray from the start index to end index.
+
+Sample Input: [1, 2, 3, 4, 5]  1 3
+Sample Output:  [1,4,9,4,5]
+
+
+Sample Input:  [2, 5, 10, 7, 5, 5, 17, 20, 15, 44, 55]  4   8
+Sample Output:  [2,5,10,7,25,25,289,400,15,44,55]
