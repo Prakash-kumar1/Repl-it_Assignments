2e347e572e5e85e56385d62ec5cce4c4b7ea7bbe
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:54:28 +0530

    Create Binary.js

---
+++ JS-Test-4/Binary.js
@@ -0,0 +1,12 @@
+Question Number -11: Given an integer array, return true if it can be used for binary search else return false
+
+Input Format : An array arr
+Output Format : Return true or false
+
+Sample Input:  [1, 5, 7, 8]
+Sample Output:  true
+
+Sample Input:  [1, 3, 2]
+Sample Output: false
+
+Note :- We can apply binary search on array if the array is in descending order as well.
