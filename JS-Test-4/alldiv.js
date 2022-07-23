3ba4a90cc721c8a60f34b035a584865425100ef8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:57:22 +0530

    Create alldiv.js

---
+++ JS-Test-4/alldiv.js
@@ -0,0 +1,11 @@
+Question Number -12: Given an integer array, find all the elements which are divisible by n and return the count
+
+Input Format: An array arr and a number n
+
+Output Format: Return the number of integers which are divisible by n
+
+Sample Input:  [1, 5, 7, 8]  3
+Sample Output: 0
+
+Sample Input: [1,5,7,8] 1
+Sample Output: 4
