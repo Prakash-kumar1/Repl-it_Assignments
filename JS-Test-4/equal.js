b610ccd44af878a3994b6f61f5ab20264a980799
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:20:13 +0530

    Check if both arrays are equal or not.

--- JS-Test-4/equal.js
+++ JS-Test-4/equal.js
@@ -11,3 +11,27 @@
 
 // Sample Input: [1, 2, 3]  [2,4,6]
 // Sample Output:  false
+
+
+function isArray(arr1, arr2){
+    let count = 0 ;
+    let arr3 = arr1.sort();
+    let arr4 = arr2.sort();
+      // console.log(arr1);
+      // console.log(arr2);
+      
+      for(let i=0; i<arr1.length; i++){
+        if(arr3[i] == arr4[i]){}
+        else{count += 1}
+      }  
+      // console.log(count);  
+      
+        if(count >= 1){
+          return false ;
+        } else {
+          return true ;
+        }
+    }
+    console.log(isArray([1, 5, 7, 8] , [5, 8, 7, 1]))
+    console.log(isArray([1, 2, 3],[2, 4, 6]))
+    exports.isArray = isArray
