1c76b1d6d96e22d0a317f2829d1a5da64c6357c4
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-23 11:55:12 +0530

    Binary Search

--- JS-Test-4/Binary.js
+++ JS-Test-4/Binary.js
@@ -1,12 +1,28 @@
-Question Number -11: Given an integer array, return true if it can be used for binary search else return false
+// Question Number -11: Given an integer array, return true if it can be used for binary search else return false
 
-Input Format : An array arr
-Output Format : Return true or false
+// Input Format : An array arr
+// Output Format : Return true or false
 
-Sample Input:  [1, 5, 7, 8]
-Sample Output:  true
+// Sample Input:  [1, 5, 7, 8]
+// Sample Output:  true
 
-Sample Input:  [1, 3, 2]
-Sample Output: false
+// Sample Input:  [1, 3, 2]
+// Sample Output: false
 
-Note :- We can apply binary search on array if the array is in descending order as well.
+// Note :- We can apply binary search on array if the array is in descending order as well.
+
+
+function binary_search(arr){
+    let element = 0;
+    if(element == 0 || element == 1){
+      return true ;
+    }
+    for(i=1;i<element;i++){
+      if(arr[i-1] > arr[i]){
+        return false ;
+      }
+    }
+    return true ;
+  }
+  console.log( binary_search([1, 5, 7, 8]))
+  exports.binary_search = binary_search
