92d7580ee251c593eff460a4351a9d0bd8543d8b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-25 16:10:48 +0530

    Count The numbers of  " E "

--- String/Epresent.js
+++ String/Epresent.js
@@ -9,3 +9,17 @@
 // Sample Output:  1
 
 
+function counte(string){
+    let Count = 0;
+   for (let i= 0; i < string.length; i++) 
+   {
+      if (string.charAt(i) == 'e') 
+        {
+        Count += 1;
+        }
+    }
+    return Count;
+  }
+  console.log(counte('aircampus'))
+  console.log(counte('help'))
+  exports.counte = counte
