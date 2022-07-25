e88bc8ae56c7de670f85610df8036a2f917fb156
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-25 15:55:12 +0530

    Create vowel.js

---
+++ String/vowel.js
@@ -0,0 +1,11 @@
+// Question no. -3 Given a string, count the number of vowels present in the string and return it.
+// Input Format : A string string
+// Output Format : Return the count of vowels in the string
+
+// Sample Input: aircampus
+// Sample Output: 4
+
+// Sample Input:  javascript
+// Sample Output: 3
+
+
