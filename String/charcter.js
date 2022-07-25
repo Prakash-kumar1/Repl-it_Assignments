4a4540cf0ad60c2e5d5b4ec9e02fc29427c9a57e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-25 15:52:08 +0530

    Create charcter.js

---
+++ String/charcter.js
@@ -0,0 +1,11 @@
+// Question no. -2 Given a string, add all the characters of the string to an array, excluding whitespaces. Return the character array
+// Input Format : A string string
+
+// Output Format : Character array without whitespaces.
+
+// Sample Input:  aircampus
+// Sample Output: ['a', 'i', 'r', 'c', 'a', 'm', 'p', 'u', 's']
+
+// Sample Input:  java script
+// Sample Output:  ['j', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
+
