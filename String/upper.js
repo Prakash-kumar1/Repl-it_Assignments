8ece1d109e23f1b61b88b3523f36ddd49681fae9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-25 15:59:10 +0530

    Change all the characters to uppercase

---
+++ String/upper.js
@@ -0,0 +1,17 @@
+// Question no. -4  Given a string, change all the characters to uppercase and return the new string.
+// Input Format :  A string string
+// Output Format : Change all the characters to uppercase and return the string.
+
+// Sample Input:  aircampus
+// Sample Output:   AIRCAMPUS
+
+// Sample Input:  Javascript
+// Sample Output:  JAVASCRIPT
+
+
+function vowels(string){
+    return (string.toUpperCase()) ;
+    }
+    console.log(vowels('aircampus'))
+    console.log(vowels('Javascript'))
+    exports.vowels = vowels
