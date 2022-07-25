e1f5ef258ef35b953457c7349c0a3a75f413961e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-25 16:10:00 +0530

    Create Epresent.js

---
+++ String/Epresent.js
@@ -0,0 +1,11 @@
+// Question no.-6 Given a string, find out how many times ‘e’ is present in the string. Return the count of ‘e.’
+// Input Format : A string string
+// Output Format :  return the count of e present in the string.
+
+// Sample Input:  aircampus
+// Sample Output:  0
+
+// Sample Input:  help
+// Sample Output:  1
+
+
