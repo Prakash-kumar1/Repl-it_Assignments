2fcfdfcfc0baad0a0eacca778cd68bf263f17f22
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:46:51 +0530

    Return the string after removing all the whitespace

--- String/remove.js
+++ String/remove.js
@@ -1,10 +1,16 @@
-Question no.-11: Given a string, remove all the whitespaces and then return the new string.
-Input Format :  Given a string str
-Output Format :  Return the string after removing all the whitespace
+// Question no.-11: Given a string, remove all the whitespaces and then return the new string.
+// Input Format :  Given a string str
+// Output Format :  Return the string after removing all the whitespace
 
-Sample Input:  " air campus "
-Sample Output:  "aircampus"
+// Sample Input:  " air campus "
+// Sample Output:  "aircampus"
 
-Sample Input:  "     java script"
-Sample Output:  javascript
+// Sample Input:  "     java script"
+// Sample Output:  javascript
 
+function removeSpace(str){
+    return str.replace(/ /g, "") ;
+   }
+   console.log(removeSpace(" air campus "))
+   console.log(removeSpace("     java script"))
+   exports.removeSpace = removeSpace
