d872df225cc8990cb877f6bc51c1c1d27bd02bad
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:53:17 +0530

    Return the string after removing all the whitespace from the end of the string

--- String/white.js
+++ String/white.js
@@ -1,10 +1,18 @@
-Question no.-13: Given a string, remove all the whitespaces from the end and then return the new string
+// Question no.-13: Given a string, remove all the whitespaces from the end and then return the new string
 
-Input Format :  Given a string str
-Output Format :  Return the string after removing all the whitespace from the end of the string
+// Input Format :  Given a string str
+// Output Format :  Return the string after removing all the whitespace from the end of the string
 
-Sample Input:  "    air campus    "
-Sample Output:  "    air campus"
+// Sample Input:  "    air campus    "
+// Sample Output:  "    air campus"
 
-Sample Input:  "     java script           "
-Sample Output:  "     java script"
+// Sample Input:  "     java script           "
+// Sample Output:  "     java script"
+
+
+function removeSpace(str){
+    return str.trimEnd() ;
+   }
+   console.log(removeSpace("    air campus    "))
+   console.log(removeSpace("     java script         "))
+   exports.removeSpace = removeSpace
