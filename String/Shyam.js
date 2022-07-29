273ad258c3ad1e7865c3c66e111e0d15dbecf233
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 22:02:35 +0530

    Replace all the occurence of Ram with Shyam and return the string

--- String/Shyam.js
+++ String/Shyam.js
@@ -1,10 +1,18 @@
-Question no.-15: Given a string, replace all the occurrences of Ram with Shyam
+// Question no.-15: Given a string, replace all the occurrences of Ram with Shyam
 
-Input Format : Given a string str
-Output Format : Replace all the occurence of Ram with Shyam and return the string
+// Input Format : Given a string str
+// Output Format : Replace all the occurence of Ram with Shyam and return the string
 
-Sample Input:  Ram owns a dog. Ram likes animals
-Sample Output:  Shyam owns a dog. Shyam likes animals
+// Sample Input:  Ram owns a dog. Ram likes animals
+// Sample Output:  Shyam owns a dog. Shyam likes animals
 
-Sample Input:  Shyam likes to code. Ram is a javascript developer.
-Sample Output: Shyam likes to code. Shyam is a javascript developer.
+// Sample Input:  Shyam likes to code. Ram is a javascript developer.
+// Sample Output: Shyam likes to code. Shyam is a javascript developer.
+
+function ramShyam(str){
+    // return str.replaceAll("Ram" , "Shyam") ;
+       // OR 
+     return str.replace(/Ram/ig , "Shyam") ;
+   }
+   console.log(ramShyam("Ram owns a dog. Ram likes animals"))
+   exports.ramShyam = ramShyam
