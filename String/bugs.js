871553868becd7e7d303deadea2b66900bd42107
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:41:40 +0530

    Calculate total number of bugs

--- String/bugs.js
+++ String/bugs.js
@@ -1,9 +1,21 @@
 // Question no.-9 : Ravi wrote two javascript programs, each programs have a and b number of bugs. Calculate total number of bugs and return "Total number of bugs are a + b". Use template literal
-Input Format :  Two integer a and b
-Output Format :  "Total number of bugs are sum of a and b"
+// Input Format :  Two integer a and b
+// Output Format :  "Total number of bugs are sum of a and b"
 
-Sample Input:  2 3
-Sample Output:  Total number of bugs are 5
+// Sample Input:  2 3
+// Sample Output:  Total number of bugs are 5
 
-Sample Input:  99 99
-Sample Output:  Total number of bugs are 198
+// Sample Input:  99 99
+// Sample Output:  Total number of bugs are 198
+
+
+function numberOfBugs(a, b){
+    // return('Total number of bugs are'+ ' ' + (a+b))
+     
+        // OR 
+     
+     return(`Total number of bugs are ${a+b}`)
+   }
+   console.log(numberOfBugs(99,99))
+   exports.numberOfBugs = numberOfBugs
+   
