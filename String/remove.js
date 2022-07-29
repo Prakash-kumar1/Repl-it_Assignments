bb2bf1dfdd23ee6897bd3b420709279a74716ab9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:46:02 +0530

    Create remove.js

---
+++ String/remove.js
@@ -0,0 +1,10 @@
+Question no.-11: Given a string, remove all the whitespaces and then return the new string.
+Input Format :  Given a string str
+Output Format :  Return the string after removing all the whitespace
+
+Sample Input:  " air campus "
+Sample Output:  "aircampus"
+
+Sample Input:  "     java script"
+Sample Output:  javascript
+
