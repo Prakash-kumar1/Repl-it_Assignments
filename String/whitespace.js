8d8035d6f19b8d5e79a310d4af501f5e745a6d8f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:50:08 +0530

    Return string after removing all the whitespace from the start of the string

--- String/whitespace.js
+++ String/whitespace.js
@@ -1,10 +1,18 @@
-Question no.-12: Given a string, remove all the whitespaces from the start and then return the new string
+// Question no.-12: Given a string, remove all the whitespaces from the start and then return the new string
 
-Input Format :  Given a string str
-Output Format :  Return string after removing all the whitespace from the start of the string
+// Input Format :  Given a string str
+// Output Format :  Return string after removing all the whitespace from the start of the string
 
-Sample Input:  " air campus "
-Sample Output:  "air campus"
+// Sample Input:  " air campus "
+// Sample Output:  "air campus"
 
-Sample Input:   "     java script"
-Sample Output:   "java script"
+// Sample Input:   "     java script"
+// Sample Output:   "java script"
+
+
+function removeSpace(str){
+    return str.trimStart() ;
+  }
+  console.log(removeSpace(" air campus "))
+  console.log(removeSpace("     java script"))
+  exports.removeSpace = removeSpace
