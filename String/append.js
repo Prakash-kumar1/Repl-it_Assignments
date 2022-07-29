bbe73e9638d203e0aa09806468ff798ae062dfee
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:37:32 +0530

    Create append.js

---
+++ String/append.js
@@ -0,0 +1,12 @@
+// Question no.-8 : Given a sentence and a word, add the given word to the end of the string.
+
+// Input Format :  Two string str1 and str2
+// Output Format :  Add str2 to end of str1 and return str1
+
+// Sample Input:  air  campus
+// Sample Output:  aircampus
+
+// Sample Input:  Java   script
+// Sample Output:  Javascript
+
+
