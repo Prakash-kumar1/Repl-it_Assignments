9c15941e7c3f54c63d03c83cf83c1ad85f7fd425
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 22:08:33 +0530

    Create seperate.js

---
+++ String/seperate.js
@@ -0,0 +1,10 @@
+Question no.-17: Given 3 strings, join string1 and string2 separated by string3. Return the final string.
+
+Input Format : Given three strings str1, str2 and str3
+Output Format :  Return the str3 seperated string.
+
+Sample Input:  air    campus    -
+Sample Output:    air - campus
+
+Sample Input:  javascript    cool     is
+Sample Output:  javascript is cool
