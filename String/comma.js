9c43c655f06cb328d1578b3e0980e722899dee33
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 22:05:26 +0530

    Return the comma seperated string.

--- String/comma.js
+++ String/comma.js
@@ -1,10 +1,30 @@
-Question no.-16: Given a strings, return all the characters of string separated by comma (,)
+// Question no.-16: Given a strings, return all the characters of string separated by comma (,)
 
-Input Format : Given a strings str
-Output Format : Return the comma seperated string.
+// Input Format : Given a strings str
+// Output Format : Return the comma seperated string.
 
-Sample Input:  aircampus
-Sample Output:  a,i,r,c,a,m,p,u,s
+// Sample Input:  aircampus
+// Sample Output:  a,i,r,c,a,m,p,u,s
 
-Sample Input:  javascript
-Sample Output:  j,a,v,a,s,c,r,i,p,t
+// Sample Input:  javascript
+// Sample Output:  j,a,v,a,s,c,r,i,p,t
+
+function commaSep(str){
+    let strans='' ;
+       for(i=0;i<str.length;i++){
+         strans=strans+','+str[i]
+       }
+        return strans.slice(1)
+   }
+   console.log(commaSep('aircampus')) ;
+   exports.commaSep = commaSep
+   
+   
+   //   OR Use this way 
+   
+   
+   function commaSep(str){
+      return str.split('').join(',')
+   }
+   console.log(commaSep('javascript')) ;
+   exports.commaSep = commaSep
