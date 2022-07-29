89078940bada27fef90e2f6e22de3ee3ceb54a8e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:49:13 +0530

    Create whitespace.js

---
+++ String/whitespace.js
@@ -0,0 +1,10 @@
+Question no.-12: Given a string, remove all the whitespaces from the start and then return the new string
+
+Input Format :  Given a string str
+Output Format :  Return string after removing all the whitespace from the start of the string
+
+Sample Input:  " air campus "
+Sample Output:  "air campus"
+
+Sample Input:   "     java script"
+Sample Output:   "java script"
