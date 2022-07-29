1da74bed927aad6c2438552b50e978011766692a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:43:32 +0530

    Create substring.js

---
+++ String/substring.js
@@ -0,0 +1,11 @@
+// Question no.-10: Given a string and two index values, return the string from the start index to end index.
+// Input Format : Given a string str and two integers a and b
+// Output Format :  Return the string from the start index to the end index.
+
+// Sample Input:  aircampus 3 9
+// Sample Output:  campus
+
+// Sample Input:  javascript 4 10
+// Sample Output:  script
+
+
