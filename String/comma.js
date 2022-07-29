7e974f3ff91553b1cf92027d102b901e43dffe57
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 22:04:28 +0530

    Create comma.js

---
+++ String/comma.js
@@ -0,0 +1,10 @@
+Question no.-16: Given a strings, return all the characters of string separated by comma (,)
+
+Input Format : Given a strings str
+Output Format : Return the comma seperated string.
+
+Sample Input:  aircampus
+Sample Output:  a,i,r,c,a,m,p,u,s
+
+Sample Input:  javascript
+Sample Output:  j,a,v,a,s,c,r,i,p,t
