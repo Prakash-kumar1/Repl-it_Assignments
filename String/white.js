500648ce4ee0e82856f398deafcbf41297496b49
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:52:27 +0530

    Create white.js

---
+++ String/white.js
@@ -0,0 +1,10 @@
+Question no.-13: Given a string, remove all the whitespaces from the end and then return the new string
+
+Input Format :  Given a string str
+Output Format :  Return the string after removing all the whitespace from the end of the string
+
+Sample Input:  "    air campus    "
+Sample Output:  "    air campus"
+
+Sample Input:  "     java script           "
+Sample Output:  "     java script"
