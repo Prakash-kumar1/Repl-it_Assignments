d61b5b0ac976c7580a916a44db784f396eea13a5
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 22:01:29 +0530

    Create Shyam.js

---
+++ String/Shyam.js
@@ -0,0 +1,10 @@
+Question no.-15: Given a string, replace all the occurrences of Ram with Shyam
+
+Input Format : Given a string str
+Output Format : Replace all the occurence of Ram with Shyam and return the string
+
+Sample Input:  Ram owns a dog. Ram likes animals
+Sample Output:  Shyam owns a dog. Shyam likes animals
+
+Sample Input:  Shyam likes to code. Ram is a javascript developer.
+Sample Output: Shyam likes to code. Shyam is a javascript developer.
