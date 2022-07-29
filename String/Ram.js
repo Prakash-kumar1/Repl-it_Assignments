96a2f09b5e14d0f1b11498c09871fb0eb7873ea8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-29 21:57:54 +0530

    Create Ram.js

---
+++ String/Ram.js
@@ -0,0 +1,10 @@
+Question no.-14:  Given a string, replace the first occurrences of Ram with Shyam
+
+Input Format :  Given a string str
+Output Format : Replace the first occurence of Ram with Shyam and return the string
+
+Sample Input:  Ram owns a dog. Ram likes animals
+Sample Output:  Shyam owns a dog. Ram likes animals
+
+Sample Input:  Shyam likes to code. Ram is a javascript developer.
+Sample Output:  Shyam likes to code. Shyam is a javascript developer.
