da1ca630230e932e3eee5cbf8b05345bd18dc11c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:43:22 +0530

    Create midchar.js

---
+++ String/midchar.js
@@ -0,0 +1,10 @@
+Question no.-26: Given a string, return the middle character, if the string length is odd, else return the middle two characters.
+
+Input Format :  Given a strings str
+Output Format :  Return the middle character if string length is odd else return middle two characters.
+
+Sample Input:  test
+Sample Output:  es
+
+Sample Input:  testing
+Sample Output: t
