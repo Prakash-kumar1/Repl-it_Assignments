fa3e44adf460906068ce11cd60c33b1e0b175f0f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:22:42 +0530

    Return the string in reverse

--- String/reverse.js
+++ String/reverse.js
@@ -1,9 +1,26 @@
-Question no.-18: Given a string, return the string in reverse
-Input Format :  Given a string str
-Output Format :  Return the string in reverse
+// Question no.-18: Given a string, return the string in reverse
+// Input Format :  Given a string str
+// Output Format :  Return the string in reverse
 
-Sample Input:  aircampus
-Sample Output:  supmacria
+// Sample Input:  aircampus
+// Sample Output:  supmacria
 
-Sample Input:  javascript
-Sample Output:  tpircsavaj
+// Sample Input:  javascript
+// Sample Output:  tpircsavaj
+
+
+function reverse(str){
+    // return str.split('').reverse().join('');
+    
+    // OR Use for loop
+      
+    let reversedName = '' ;
+    for(let i=0; i<str.length; i++){
+        reversedName = str[i] + reversedName ;
+    }
+    return(reversedName) ;
+    }
+    
+    console.log(reverse('aircampus'));
+    console.log(reverse('javascript'));
+    exports.reverse = reverse
