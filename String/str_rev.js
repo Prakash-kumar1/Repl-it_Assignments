11ed39ca97a12efcced09d540334d5b29f0af3fc
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:35:17 +0530

    Create str_rev.js

---
+++ String/str_rev.js
@@ -0,0 +1,10 @@
+Question no.-23: Given a string sentence, reverse the string word by word
+
+Input Format  :  Given a strings str
+Output Format  :  Reveres the given string word by word then return the reversed string
+
+Sample Input:   the sky is blue
+Sample Output:   blue is sky the
+
+Sample Input:   hello   world!
+Sample Output:  world! hello
