27e27d69f3fc5fb97e2ba28d519709c4a7e82531
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:32:31 +0530

    Create zeroes.js

---
+++ String/zeroes.js
@@ -0,0 +1,10 @@
+Question no.-22:  Given a string remove leading and trailing zeros from it and return the string
+
+Input Format  :  Given a strings str
+Output Format  :  Return the string after removing all the leading and trailing zeroes
+
+Sample Input:   230.00
+Sample Output:  230
+
+Sample Input:   00420
+Sample Output:  420
