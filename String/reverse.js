78a700ac50c5871315ca57939588251e854bd0a2
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:20:56 +0530

    Create reverse.js

---
+++ String/reverse.js
@@ -0,0 +1,9 @@
+Question no.-18: Given a string, return the string in reverse
+Input Format :  Given a string str
+Output Format :  Return the string in reverse
+
+Sample Input:  aircampus
+Sample Output:  supmacria
+
+Sample Input:  javascript
+Sample Output:  tpircsavaj
