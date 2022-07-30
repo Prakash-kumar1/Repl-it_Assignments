144d3bdc9234e2b525766f19a3007660889db8e0
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:28:49 +0530

    Create alphanumeric.js

---
+++ String/alphanumeric.js
@@ -0,0 +1,15 @@
+Given a string, if the string is alphanumeric return true else return false.
+Input Format
+Given a strings str
+
+Output Format
+Return true if the given strings is alphanumeric else return false
+
+Sample Input:
+aircampus
+Sample Output:
+false
+Sample Input:
+javascr1pt
+Sample Output:
+true
