c9f8b45f12ce7bab4740d11ae56a45eade80a2d1
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:26:28 +0530

    Create palindrome.js

---
+++ String/palindrome.js
@@ -0,0 +1,15 @@
+Question no.-20:  Given a string, if it is a palindrome return true else return false.
+Input Format
+Given a strings str
+
+Output Format
+Return true if the given strings is palindrome else return false
+
+Sample Input:
+arora
+Sample Output:
+true
+Sample Input:
+aircampus
+Sample Output:
+false
