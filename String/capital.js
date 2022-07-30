feb729829c38ffb6c17ce04588ee54dde77c7403
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:38:52 +0530

    Capitalize the first letter of each word then return the string

--- String/capital.js
+++ String/capital.js
@@ -1,15 +1,23 @@
-Question no.-24:  Given a string, capitalize the first letter of each word.
-Input Format
-Given a strings str
+// Question no.-24:  Given a string, capitalize the first letter of each word.
 
-Output Format
-Capitalize the first letter of each word then return the string.
+// Input Format :  Given a strings str
+// Output Format  :   Capitalize the first letter of each word then return the string.
 
-Sample Input:
-This is a title
-Sample Output:
-This Is A Title
-Sample Input:
-capitalize every word
-Sample Output:
-Capitalize Every 
+// Sample Input:   This is a title
+// Sample Output:  This Is A Title
+
+// Sample Input:   capitalize every word
+// Sample Output:   Capitalize Every word
+
+
+function capitalization(str){
+    let words = str.split(" ");
+    var CapitalizedWords = [];  
+        words.forEach(element => {  
+            CapitalizedWords.push(element[0].toUpperCase() + element.slice(1, element.length));  
+        });  
+        return CapitalizedWords.join(' ');  
+    }
+    console.log(capitalization('This is a title'))
+    console.log(capitalization('capitalize every word'))
+    exports.capitalization = capitalization
