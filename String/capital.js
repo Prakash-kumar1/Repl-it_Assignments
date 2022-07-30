7d8a3acd8ef947585e78c9aca30f55bcbae814e8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:37:22 +0530

    Create capital.js

---
+++ String/capital.js
@@ -0,0 +1,15 @@
+Question no.-24:  Given a string, capitalize the first letter of each word.
+Input Format
+Given a strings str
+
+Output Format
+Capitalize the first letter of each word then return the string.
+
+Sample Input:
+This is a title
+Sample Output:
+This Is A Title
+Sample Input:
+capitalize every word
+Sample Output:
+Capitalize Every 
