fefc252fd0e6d7e3b1d8f606f587ad1447b7902a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:46:24 +0530

    Create email.js

---
+++ String/email.js
@@ -0,0 +1,10 @@
+Question no.-27:  Given a string which contains an email address, return true if the given email is valid, else return false.
+
+Input Format :  Given a strings str
+Output Format  :  Return true if the email is valid else return false
+
+Sample Input:  js@aircampus.in
+Sample Output:  true
+
+Sample Input:   admin@aircampus
+Sample Output:   false
