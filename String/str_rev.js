2033dd6473b37e5be343e97c99a6ca04a902d44f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:36:22 +0530

    Reveres the given string word by word then return the reversed string

--- String/str_rev.js
+++ String/str_rev.js
@@ -1,10 +1,17 @@
-Question no.-23: Given a string sentence, reverse the string word by word
+// Question no.-23: Given a string sentence, reverse the string word by word
 
-Input Format  :  Given a strings str
-Output Format  :  Reveres the given string word by word then return the reversed string
+// Input Format  :  Given a strings str
+// Output Format  :  Reveres the given string word by word then return the reversed string
 
-Sample Input:   the sky is blue
-Sample Output:   blue is sky the
+// Sample Input:   the sky is blue
+// Sample Output:   blue is sky the
 
-Sample Input:   hello   world!
-Sample Output:  world! hello
+// Sample Input:   hello   world!
+// Sample Output:  world! hello
+
+
+function reverse(str){
+    return  str.split(' ').reverse().join(' ')
+    }
+    console.log(reverse("the sky is blue"));
+    exports.reverse = reverse
