cd49ac9f72b79986a07cae5dea9252b98fb867e0
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:40:27 +0530

    Create put.js

---
+++ String/put.js
@@ -0,0 +1,10 @@
+Question no.-25:  Given a number, format the given number by placing commas after thousand place, lakh place, etc. Return the formatted string.
+
+Input Format  :  Given a strings str
+Output Format :  Return the formatted string
+
+Sample Input:  100000
+Sample Output:  1,00,000
+
+Sample Input:  87678567465996
+Sample Output:  8,76,78,56,74,65,996
