df3017d0773c06ffd9e37fdc3d88d08a6589f14e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:24:33 +0530

    Create anagram.js

---
+++ String/anagram.js
@@ -0,0 +1,9 @@
+Question no.-19: Given two strings, check if they are anagrams or not. If they are anagrams return true else return false.
+Input Format :  Given two strings str1 and str2
+Output Format :  Return the true if the given strings are anagrams else return false
+
+Sample Input:   elbow     below
+Sample Output:   true
+
+Sample Input:  night       thin
+Sample Output:   false
