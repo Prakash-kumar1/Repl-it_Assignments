94e65a6034b2c82532dbadc75efc76e9a9d1e433
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:30:43 +0530

    Return true if the given strings is alphanumeric else return false

--- String/alphanumeric.js
+++ String/alphanumeric.js
@@ -1,15 +1,28 @@
-Given a string, if the string is alphanumeric return true else return false.
-Input Format
-Given a strings str
+// Question no.-21: Given a string, if the string is alphanumeric return true else return false.
 
-Output Format
-Return true if the given strings is alphanumeric else return false
+// Input Format :  Given a strings str
+// Output Format :  Return true if the given strings is alphanumeric else return false
 
-Sample Input:
-aircampus
-Sample Output:
-false
-Sample Input:
-javascr1pt
-Sample Output:
-true
+// Sample Input:  aircampus
+// Sample Output:  false
+
+// Sample Input:  javascr1pt
+// Sample Output:  true
+
+
+function alphanumeric(str){
+    for(let i=0;i<str.length;i++){
+    let check= str.split('').sort().join()
+    if((check.charCodeAt(i)>47 && check.charCodeAt(i) <58)){
+          return true ;
+        }
+        else {
+          return false ;
+        }
+        // value++
+    }
+    
+    }
+    console.log(alphanumeric("aircampus"))
+    console.log(alphanumeric("javascr1pt"))
+    exports.alphanumeric = alphanumeric
