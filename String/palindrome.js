24c001ef9824b244c9a6b65f17f3545d87fd6f3e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:28:03 +0530

    Return true if the given strings is palindrome else return false

--- String/palindrome.js
+++ String/palindrome.js
@@ -1,15 +1,24 @@
-Question no.-20:  Given a string, if it is a palindrome return true else return false.
-Input Format
-Given a strings str
+// Question no.-20:  Given a string, if it is a palindrome return true else return false.
 
-Output Format
-Return true if the given strings is palindrome else return false
+// Input Format :  Given a strings str
+// Output Format :  Return true if the given strings is palindrome else return false
 
-Sample Input:
-arora
-Sample Output:
-true
-Sample Input:
-aircampus
-Sample Output:
-false
+// Sample Input:  arora
+// Sample Output:  true
+
+// Sample Input:  aircampus
+// Sample Output: false
+
+
+function palindrome(str){
+    for (let i = 0; i < str.length; i++) {
+    // check if first and last string are same
+      if (str[i] !== str[str.length - 1 - i]) {
+        return false ;
+     }
+     }
+        return true;
+    }
+    console.log( palindrome("arora"))
+    console.log( palindrome("aircampus"))
+    exports.palindrome = palindrome
