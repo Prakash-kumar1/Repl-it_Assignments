229e5367e69435a5d5f84fdef99e5cfdee99a258
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-30 11:48:12 +0530

    Return true if the email is valid else return false

--- String/email.js
+++ String/email.js
@@ -1,10 +1,34 @@
-Question no.-27:  Given a string which contains an email address, return true if the given email is valid, else return false.
+// Question no.-27:  Given a string which contains an email address, return true if the given email is valid, else return false.
 
-Input Format :  Given a strings str
-Output Format  :  Return true if the email is valid else return false
+// Input Format :  Given a strings str
+// Output Format  :  Return true if the email is valid else return false
 
-Sample Input:  js@aircampus.in
-Sample Output:  true
+// Sample Input:  js@aircampus.in
+// Sample Output:  true
 
-Sample Input:   admin@aircampus
-Sample Output:   false
+// Sample Input:   admin@aircampus
+// Sample Output:   false
+
+
+// function validEmail(str){
+// if(str.endsWith('@aircampus.in')){
+//     return true ;
+//   }
+//     return false ;
+//   }
+// console.log(validEmail('admin@aircampus'))
+// exports.validEmail = validEmail
+
+
+function validEmail(str){
+    let pos = str.indexOf('@') ;
+    let dot = str.indexOf('.') ;
+    
+      if(pos<1 || dot < pos+2){
+        return false
+      }
+      return true
+    }
+    console.log(validEmail('admin@aircampus'))
+    console.log(validEmail('js@aircampus.in'))
+    exports.validEmail = validEmail
