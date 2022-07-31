6fbadac6b497a994f8213e79dabc78ffa2feb587
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-31 21:24:31 +0530

    Create delievery.js

---
+++ Object/delievery.js
@@ -0,0 +1,10 @@
+Question Number-6: Given an object containing a list of items present in the cart. Find if the order is eligible for free delivery or not. If the order amount is greater than $50.00 it is eligible for free delivery
+
+Input Format  :  An object obj containing list of items and their prices
+Output Format  :  Return true if order is eligible for free delivery else return false
+
+Sample Input:   { "Shampoo": 5.99, "Rubber Ducks": 15.99 }
+Sample Output:  false
+
+Sample Input:  { "Monopoly": 11.99, "Secret Hitler": 35.99, "Bananagrams": 13.99 }
+Sample Output:  true
