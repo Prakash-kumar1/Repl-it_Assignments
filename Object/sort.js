f5adaae2b6f0bacbf95317dda4c4e0b5e589b122
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-31 21:30:14 +0530

    Create sort.js

---
+++ Object/sort.js
@@ -0,0 +1,17 @@
+Given an array of objects containing drink and their prices, return the array such that the object contains drink with lowest price first.
+Input Format
+An object obj
+
+Output Format
+Return array such that drinks object is in sorted order on the basis of their price.
+
+Sample Input:
+[
+  {name: "lemonade", price: 50},
+  {name: "lime", price: 10}
+]
+Sample Output:
+[
+  {name: "lime", price: 10},
+  {name: "lemonade", price: 50}
+]
