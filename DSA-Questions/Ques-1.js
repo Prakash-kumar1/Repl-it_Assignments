9834ea339024ba3a18a2116abeb4fadcd2d35ace
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-26 16:52:12 +0530

    return the index where it would be if it were inserted in order.

---
+++ DSA-Questions/Ques-1.js
@@ -0,0 +1,17 @@
+// Question => 1 : Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+
+function searchInsert(nums, target){
+    for(let i=0; i<nums.length; i++){
+        if(nums.includes(target)){
+            res = (nums.indexOf(target));
+        }else if(!nums.includes(target)){
+            nums.push(target);
+            nums.sort(function(a, b){return a - b});
+            res = (nums.indexOf(target));
+        };
+    };
+    return (res);
+};
+console.log(searchInsert([1,3,5,6],7));
+
