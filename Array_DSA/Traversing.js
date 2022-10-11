5783d54caca994eaee3438642e81d444326ec881
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-11 12:35:29 +0530

    Traversing  The  Array

---
+++ Array_DSA/Traversing.js
@@ -0,0 +1,11 @@
+// Question no-1 : Print each Number in [1,23,45,67,54,80,35,27,29,77] ??
+
+
+const data = [1,23,45,67,54,80,35,27,29,77]
+
+for(let i=0 ; i<data.length ; i++){
+    console.log(i)    //it gives Index values
+    console.log(data[i])    //it gives  values
+    console.log(`Array ${i} is ${data[i]} `)
+}
+
